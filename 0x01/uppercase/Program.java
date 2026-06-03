67b7ad9856653c4480f9504025624772a098120c
player0-glitch <mokhaditlhalefo@gmail.com>
2026-06-03 15:20:16 +0200

    java number stuff

---
+++ 0x01/uppercase/Program.java
@@ -0,0 +1,17 @@
+
+public class Program {
+  public static void main(String[] args) {
+
+    boolean characterIsUppsercase1 = Character.IsUpper('a');
+    boolean characterIsUppsercase2 = Character.IsUpper('B');
+    boolean characterIsUppsercase3 = Character.IsUpper('d');
+    boolean characterIsUppsercase4 = Character.IsUpper('f');
+    boolean characterIsUppsercase5 = Character.IsUpper('G');
+
+    System.out.println("%b\n" + characterIsUppsercase1);
+    System.out.println("%b\n" + characterIsUppsercase2);
+    System.out.println("%b\n" + characterIsUppsercase3);
+    System.out.println("%b\n" + characterIsUppsercase4);
+    System.out.println("%b\n" + characterIsUppsercase5);
+  }
+}
