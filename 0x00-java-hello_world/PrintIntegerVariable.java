7529b14f5cc5c34f354a7b1df8cc2247df8802b9
player0-glitch <mokhaditlhalefo@gmail.com>
2026-06-03 14:40:50 +0200

    java stuff

--- 0x00-java-hello_world/PrintIntegerVariable.java
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class PrintIntegerVariable {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    Integer number = scanner.nextInt();
+    int number = scanner.nextInt();
 
     System.out.printf("Pennsylvania Avenue, %d\n", number);
     scanner.close();
--- 0x00-java-hello_world/PrintStringConcat.java
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -1,4 +1,4 @@
-public class PrintConcatString {
+public class PrintStringConcat {
   public static void main(String[] args) {
     String text1 = "Holberton";
     String text2 = "School";
