0a5e51b36cd4fc24c49227a26db8e7f1cf5bdb95
player0-glitch <mokhaditlhalefo@gmail.com>
2026-06-03 20:03:13 +0200

    java number stuff

--- 0x02/square/Square.java
+++ 0x02/square/Square.java
@@ -1,7 +1,7 @@
 public class Square {
   public static double area(double side) {
     if (side < 0)
-      throw new IllegalArgumentException("Side must have a positive value .");
+      throw new IllegalArgumentException("Side must have a positive value");
     else {
       return side * side;
     }
