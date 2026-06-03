807d63ae2983034278771d4ca2be5825440a7ace
player0-glitch <mokhaditlhalefo@gmail.com>
2026-06-03 14:37:21 +0200

    java stuff

---
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class PrintStringVariable {
+  public static void main(String[] args) {
+    String text = "Holberton School";
+    System.out.printf("%s\n", text);
+    System.out.printf("%s\n", text);
+    System.out.printf("%s\n", text.substring(0, 9));
+  }
+}
