5c70c96cef721e45380fc6e861091d13328848e6
player0-glitch <mokhaditlhalefo@gmail.com>
2026-06-03 14:39:16 +0200

    java stuff

---
+++ 0x00-java-hello_world/PrintConcatString.java
@@ -0,0 +1,8 @@
+public class PrintConcatString {
+  public static void main(String[] args) {
+    String text1 = "Holberton";
+    String text2 = "School";
+    String s = "Welcome to " + text1 + " " + text2 + "!";
+    System.out.println(s);
+  }
+}
