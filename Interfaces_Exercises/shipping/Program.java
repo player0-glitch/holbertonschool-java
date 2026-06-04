f7f3a8739fab8fe9f5a37fff6da5c29b9ee663de
player0-glitch <mokhaditlhalefo@gmail.com>
2026-06-04 15:32:07 +0200

    java

--- Interfaces_Exercises/shipping/Program.java
+++ Interfaces_Exercises/shipping/Program.java
@@ -10,12 +10,12 @@ public class Program {
     OrderProcessor orderProcessorFedex = new OrderProcessor(new Fedex());
     orderProcessorFedex.process(order1);
 
-    System.out.printf("%s - %.2f\n", order1.getShipping().getShippingProviderType(), order1.getShipping().getValue());
+    System.out.printf("%s - %,2f\n", order1.getShipping().getShippingProviderType(), order1.getShipping().getValue());
 
     OrderProcessor orderProcessorUnitedParcelService = new OrderProcessor(new UnitedParcelService());
     orderProcessorUnitedParcelService.process(order1);
 
-    System.out.printf("%s - %.2f\n", order1.getShipping().getShippingProviderType(), order1.getShipping().getValue());
+    System.out.printf("%s - %,2f\n", order1.getShipping().getShippingProviderType(), order1.getShipping().getValue());
 
     OrderProcessor orderProcessorDalseyHillblomLynn = new OrderProcessor(new DalseyHillblomLynn());
     orderProcessorDalseyHillblomLynn.process(order1);
@@ -25,34 +25,34 @@ public class Program {
     System.out.println("-----");
 
     orderProcessorFedex.process(order2);
-    System.out.printf("%s - %.2f\n", order2.getShipping().getShippingProviderType(), order2.getShipping().getValue());
+    System.out.printf("%s - %,2f\n", order2.getShipping().getShippingProviderType(), order2.getShipping().getValue());
 
     orderProcessorUnitedParcelService.process(order2);
-    System.out.printf("%s - %.2f\n", order2.getShipping().getShippingProviderType(), order2.getShipping().getValue());
+    System.out.printf("%s - %,2f\n", order2.getShipping().getShippingProviderType(), order2.getShipping().getValue());
 
     orderProcessorDalseyHillblomLynn.process(order2);
-    System.out.printf("%s - %.2f\n", order2.getShipping().getShippingProviderType(), order2.getShipping().getValue());
+    System.out.printf("%s - %,2f\n", order2.getShipping().getShippingProviderType(), order2.getShipping().getValue());
 
     System.out.println("-----");
 
     orderProcessorFedex.process(order3);
-    System.out.printf("%s - %.2f\n", order3.getShipping().getShippingProviderType(), order3.getShipping().getValue());
+    System.out.printf("%s - %,2f\n", order3.getShipping().getShippingProviderType(), order3.getShipping().getValue());
 
     orderProcessorUnitedParcelService.process(order3);
-    System.out.printf("%s - %.2f\n", order3.getShipping().getShippingProviderType(), order3.getShipping().getValue());
+    System.out.printf("%s - %,2f\n", order3.getShipping().getShippingProviderType(), order3.getShipping().getValue());
 
     orderProcessorDalseyHillblomLynn.process(order3);
-    System.out.printf("%s - %.2f\n", order3.getShipping().getShippingProviderType(), order3.getShipping().getValue());
+    System.out.printf("%s - %,2f\n", order3.getShipping().getShippingProviderType(), order3.getShipping().getValue());
 
     System.out.println("-----");
 
     orderProcessorFedex.process(order4);
-    System.out.printf("%s - %.2f\n", order4.getShipping().getShippingProviderType(), order4.getShipping().getValue());
+    System.out.printf("%s - %,2f\n", order4.getShipping().getShippingProviderType(), order4.getShipping().getValue());
 
     orderProcessorUnitedParcelService.process(order4);
-    System.out.printf("%s - %.2f\n", order4.getShipping().getShippingProviderType(), order4.getShipping().getValue());
+    System.out.printf("%s - %,2f\n", order4.getShipping().getShippingProviderType(), order4.getShipping().getValue());
 
     orderProcessorDalseyHillblomLynn.process(order4);
-    System.out.printf("%s - %.2f\n", order4.getShipping().getShippingProviderType(), order4.getShipping().getValue());
+    System.out.printf("%s - %,2f\n", order4.getShipping().getShippingProviderType(), order4.getShipping().getValue());
   }
 }
