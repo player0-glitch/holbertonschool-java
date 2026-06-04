c7ccf379f85dbdc5bf0fd94e45fe4b682bbb64ca
player0-glitch <mokhaditlhalefo@gmail.com>
2026-06-04 07:34:15 +0200

    java 0x03 exceptions

---
+++ 0x03/basic_account/BasicBankAccount.java
@@ -0,0 +1,61 @@
+import exceptions.InvalidOperationException;
+
+public class BasicBankAccount {
+  private String accountNumber;
+  private double balance;
+  private double annualInterestRate;
+
+  public BasicBankAccount(String accountNumber, double annualInterestRate)
+      throws InvalidOperationException {
+    this.accountNumber = accountNumber;
+    this.balance = 0.0;
+    this.annualInterestRate = annualInterestRate;
+  }
+
+  public String getAccountNumber() {
+    return this.accountNumber;
+  }
+
+  public double getAnnualInterestRate() {
+    return this.annualInterestRate;
+  }
+
+  public double getBalance() {
+    return this.balance;
+  }
+
+  public void deposit(double value) throws InvalidOperationException {
+    if (value <= 0) {
+      throw new InvalidOperationException("Deposit amount must be greater than zero.");
+    }
+    this.balance += value;
+  }
+
+  public double calculateMonthlyFee() {
+    double tenPercent = this.balance * 0.1;
+    return Math.min(10.0, tenPercent);
+  }
+
+  public void withdraw(double value) throws InvalidOperationException {
+    if (value <= 0) {
+      throw new InvalidOperationException("Withdrawal amount must be greater than zero.");
+    }
+    if (value > this.balance) {
+      throw new InvalidOperationException("Withdrawal amount must be less than current balance ");
+    }
+    this.balance -= value;
+  }
+
+  public double calculateMonthlyInterest() {
+    if (this.balance <= 0)
+      return 0.0;
+    double monthlyInterestRate = (this.annualInterestRate / 12) * 100;
+    return this.balance * monthlyInterestRate;
+  }
+
+  public void applyMonthlyUpdate() {
+    double fee = calculateMonthlyFee();
+    double interest = calculateMonthlyInterest();
+    balance = balance - fee + interest;
+  }
+}
