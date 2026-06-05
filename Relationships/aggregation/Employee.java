d39b88143a0a79427eb009ff3bb0a21bdd3c893d
Tlhalefo Mokhadi <TMokhadi@SINGULAR.CO.ZA>
2026-06-05 12:07:16 +0200

    java aggregation

--- Relationships/aggregation/Employee.java
+++ Relationships/aggregation/Employee.java
@@ -9,6 +9,14 @@ public class Employee {
         this.address = address;
     }
 
+    public void introduce()
+    {
+      System.out.printf("Code: %s\n",this.code);
+      System.out.printf("Name: %s\n",this.name);
+      System.out.printf("District: %s\n",this.address.getDistrict());
+      System.out.printf("City: %s\n",this.address.getCity());
+      System.out.printf("country: %s\n",this.address.getCcountry());
+    }
     public String getName() { return name; }
     public int getCode() { return code; }
     public Address getAddress() { return address; }
