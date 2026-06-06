9d74a4c18cb2e13bff09ddc984718b00f7fc2cd7
player0-glitch <mokhaditlhalefo@gmail.com>
2026-06-06 11:42:41 +0200

    java

--- Relationships/aggregation/Employee.java
+++ Relationships/aggregation/Employee.java
@@ -1,23 +1,31 @@
 public class Employee {
-    private String name;
-    private int code;
-    private Address address;
+  private String name;
+  private int code;
+  private Address address;
 
-    public Employee(String name, int code, Address address) {
-        this.name = name;
-        this.code = code;
-        this.address = address;
-    }
+  public Employee(String name, int code, Address address) {
+    this.name = name;
+    this.code = code;
+    this.address = address;
+  }
 
-    public void introduce()
-    {
-      System.out.printf("Code: %s\n",this.code);
-      System.out.printf("Name: %s\n",this.name);
-      System.out.printf("District: %s\n",this.address.getDistrict());
-      System.out.printf("City: %s\n",this.address.getCity());
-      System.out.printf("country: %s\n",this.address.getCcountry());
-    }
-    public String getName() { return name; }
-    public int getCode() { return code; }
-    public Address getAddress() { return address; }
+  public void introduce() {
+    System.out.printf("Code: %s\n", this.code);
+    System.out.printf("Name: %s\n", this.name);
+    System.out.printf("District: %s\n", this.address.getDistrict());
+    System.out.printf("City: %s\n", this.address.getCity());
+    System.out.printf("country: %s\n", this.address.getCountry());
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getCode() {
+    return code;
+  }
+
+  public Address getAddress() {
+    return address;
+  }
 }
