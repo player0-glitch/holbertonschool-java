526c284fcc193a553f70622ab5b7c64c7c231f1f
player0-glitch <mokhaditlhalefo@gmail.com>
2026-06-06 15:02:53 +0200

    java

---
+++ List/todo/Task.java
@@ -0,0 +1,34 @@
+public class Task {
+  private String description;
+  private boolean isDone;
+  private int identifier;
+
+  public Task(String description, int identifier) {
+    this.isDone = false;
+    this.identifier = identifier;
+
+    modifyDescription(description);
+  }
+
+  public String getDescription() {
+    return this.description;
+  }
+
+  public void setDone(boolean isDone) {
+    this.isDone = isDone;
+  }
+
+  public boolean getIsDone() {
+    return this.isDone;
+  }
+
+  public int getIdentifier() {
+    return this.identifier;
+  }
+
+  public void modifyDescription(String newDescription) {
+    if (newDescription == null || newDescription.trim().isEmpty())
+      throw new IllegalArgumentException("Invalid task description");
+    this.description = newDescription;
+  }
+}
---
+++ List/todo/TodoList.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoList {
+  private final List<Task> tasks;
+
+  public TodoList() {
+    this.tasks = new ArrayList<>();
+  }
+
+  public void addTask(Task task) {
+    if (task == null)
+      return;
+
+    for (Task t : tasks) {
+      if (t.getIdentifier() == task.getIdentifier())
+        throw new IllegalArgumentException("Task with identifier " + t.getIdentifier() + " already exists in the list");
+
+      this.tasks.add(task);
+    }
+  }
+
+  public boolean markTaskDone(int identifier) {
+    for (Task t : tasks) {
+      if (t.getIdentifier() == identifier) {
+        t.setDone(true);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public boolean undoTask(int identifier) {
+    for (Task t : tasks) {
+      if (t.getIdentifier() == identifier) {
+        t.setDone(false);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public void undoAll() {
+    for (Task t : tasks) {
+      t.setDone(false);
+    }
+  }
+
+  public void completeAll() {
+    for (Task t : tasks) {
+      t.setDone(true);
+    }
+  }
+
+  public void listTasks() {
+    for (Task t : tasks) {
+      String checkmark = t.getIsDone() ? "X" : " ";
+      System.out.printf("[%s] Id: %d - Description: %s%n", checkmark, t.getIdentifier(), t.getDescription());
+    }
+  }
+}
