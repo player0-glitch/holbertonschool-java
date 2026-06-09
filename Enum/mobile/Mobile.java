38b5e271d5dd43ba602cc62d47e955a9d683c273
player0-glitch <mokhaditlhalefo@gmail.com>
2026-06-09 21:14:14 +0200

    java

--- Enum/mobile/Mobile.java
+++ Enum/mobile/Mobile.java
@@ -27,25 +27,25 @@ public class Mobile {
     int oldPosition = getContactPosition(oldContact.getName());
 
     if (oldPosition == -1) {
-      throw new Exception("Could not modify contact. Contact does not exist.");
+      throw new Exception("Could not modify contact. Contact does not exist");
     }
 
     int newPosition = getContactPosition(newContact.getName());
     if (newPosition >= 0 && newPosition != oldPosition) {
-      throw new Exception("Could not modify contact. Contact with this name already exists.");
+      throw new Exception("Could not modify contact. Contact with this name already exists");
     }
 
     contactList.set(oldPosition, newContact);
-    System.out.println("Contact " + oldContact.getName() + " was updated to " + newContact.getName() + ".");
+    System.out.println("Contact " + oldContact.getName() + " was updated to " + newContact.getName());
   }
 
   public void removeContact(Contact contact) throws Exception {
     int position = getContactPosition(contact.getName());
     if (position == -1) {
-      throw new Exception("Could not remove contact. Contact does not exist.");
+      throw new Exception("Could not remove contact. Contact does not exist");
     }
     contactList.remove(position);
-    System.out.println("Contact " + contact.getName() + " was successfully removed.");
+    System.out.println("Contact " + contact.getName() + " was successfully removed");
   }
 
   public void listContacts() {
