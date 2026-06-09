c95160497b117b5bc96d2c54d1cdc1963b0deab5
player0-glitch <mokhaditlhalefo@gmail.com>
2026-06-09 21:17:37 +0200

    java

--- Enum/mobile/Mobile.java
+++ Enum/mobile/Mobile.java
@@ -17,7 +17,7 @@ public class Mobile {
 
   public void addContact(Contact contact) throws Exception {
     if (getContactPosition(contact.getName()) >= 0)
-      throw new Exception("Could not add contact. Contact with this name already exists");
+      throw new Exception("Could not add contact. Contact with this name already exists .");
 
     contactList.add(contact);
     System.out.println("Contact " + contact.getName() + " added successfully");
@@ -27,30 +27,30 @@ public class Mobile {
     int oldPosition = getContactPosition(oldContact.getName());
 
     if (oldPosition == -1) {
-      throw new Exception("Could not modify contact. Contact does not exist");
+      throw new Exception("Could not modify contact. Contact does not exist .");
     }
 
     int newPosition = getContactPosition(newContact.getName());
     if (newPosition >= 0 && newPosition != oldPosition) {
-      throw new Exception("Could not modify contact. Contact with this name already exists");
+      throw new Exception("Could not modify contact. Contact with this name already exists .");
     }
 
     contactList.set(oldPosition, newContact);
-    System.out.println("Contact " + oldContact.getName() + " was updated to " + newContact.getName());
+    System.out.println("Contact " + oldContact.getName() + " was updated to " + newContact.getName() + " .");
   }
 
   public void removeContact(Contact contact) throws Exception {
     int position = getContactPosition(contact.getName());
     if (position == -1) {
-      throw new Exception("Could not remove contact. Contact does not exist");
+      throw new Exception("Could not remove contact. Contact does not exist .");
     }
     contactList.remove(position);
-    System.out.println("Contact " + contact.getName() + " was successfully removed");
+    System.out.println("Contact " + contact.getName() + " was successfully removed .");
   }
 
   public void listContacts() {
     if (contactList.isEmpty()) {
-      System.out.println("Your contact list is empty");
+      // System.out.println("Your contact list is empty");
       return;
     }
     // System.out.println("--- Contact List ---");
